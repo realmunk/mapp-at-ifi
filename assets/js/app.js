2a17439f880d9f800a96ed984ce6b4e9bde454f6
Kristian Saksvik Munkvold <ksm@computas.no>
2013-10-30 19:11:44 +0100

    Minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,34 +4,41 @@
   var ns = window.mapp;
 
   ns.Application = function () {
-    var self = this, 
-    app = $.sammy('#application');
+    var self = this,
+    app = $.sammy('#application'); // sammy is a routing tool
+    
+    self.breadcrumb = ko.observable("");
 
     app.get('#/', function () {
+      self.breadcrumb("");
       this.load("views/menu.html", function (resp) {
         $("#application").html(resp);
       })
     });
 
     app.get('#/romoversikt', function () {
+      self.breadcrumb("/ Romoversikt");
       this.load("views/romoversikt.html", function (resp) {
         $("#application").html(resp);
       });
     });
 
     app.get('#/kart', function () {
+      self.breadcrumb("/ Kart");
       this.load("views/kart.html", function (resp) {
         $("#application").html(resp);
       });
     });
 
     app.get('#/hvaSkjer', function () {
+      self.breadcrumb("/ Hva skjer?");
       this.load("views/hvaSkjer.html", function (resp) {
         $("#application").html(resp);
       });
     });
 
     app.get('#/praktiskInfo', function () {
+      self.breadcrumb("/ Praktisk informasjon");
       this.load("views/praktiskInfo.html", function (resp) {
         $("#application").html(resp);
       });
@@ -41,7 +48,6 @@
     (function () {
       app.run("#/");
     }())
-
   }
 
 }(window, jQuery));
--- assets/js/init.js
+++ assets/js/init.js
@@ -2,6 +2,7 @@
 
   $(document).ready(function () {
     var app = new window.mapp.Application();
+    ko.applyBindings(app, $("#navbar").get(0));
   });
 
 }(window, jQuery));
