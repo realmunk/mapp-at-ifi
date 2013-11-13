f2e0e49c8907ccff9b2a25a75b5dea1087f3d02c
Amund Øgar Meisal <amundmeisal@gmail.com>
2013-11-13 16:43:22 +0100

    Starting to work

--- assets/js/EventsViewModel.js
+++ assets/js/EventsViewModel.js
@@ -9,6 +9,20 @@
 
  		self.events = ko.observableArray();
 
+
+    self.restOfWeek = ko.computed(function(){
+      events = self.events();
+
+      for(var i = 0; i < events.length; i++){
+      var date = events[i].date;
+      console.log(date);
+        
+
+      }
+      return events;
+
+    });
+
     function success(resp) {
       $.each(resp.events, function (index, event) {
         self.events.push(new model.Event(event.date, event.desc, event.where));
@@ -24,4 +38,4 @@
         .fail(error);
     }());
   };
-});
+}(window, jQuery));
--- assets/js/init.js
+++ assets/js/init.js
@@ -2,7 +2,7 @@
   $(document).ready(function () {
     var viewModels = {
       "rooms": new window.MAPP.RoomsViewModel(),
-      //"events": new window.MAPP.EventsViewModel(),
+      "events": new window.MAPP.EventsViewModel()
       },
       app = new window.MAPP.Application(viewModels);
     ko.applyBindings(app, $("#navbar").get(0));
